9629582187b6cdde11eaf77496fd90c9b6854518
BAE-Chris_McQuiggan <lshilling@hotmail.co.uk>
2019-06-04 16:15:25 +0100

    updated

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -4,8 +4,51 @@ public class Account {
 	
 	//This class needs to have:
 	//An id
+	private int id;
 	//An Account Number
+	private int accountNumber;
 	//A First Name
+	private String firstName;
 	//A last Name
+	private String lastName;
+	
+	public Account(int id, int accountNumber, String firstName, String lastName) {
+		this.id = id;
+		this.accountNumber = accountNumber;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 	
 }
--- src/main/java/com/qa/persistence/repository/AccountMapRepository.java
+++ src/main/java/com/qa/persistence/repository/AccountMapRepository.java
@@ -4,37 +4,45 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.qa.persistence.domain.Account;
+import com.qa.util.JSONUtil;
 
-public class AccountMapRepository implements AccountRepository{
-	
+public class AccountMapRepository implements AccountRepository {
+
 	Map<Integer, Account> accountMap = new HashMap<Integer, Account>();
-	
-	//You must provide concrete implementation for each of these methods
-	//do not change the method signature
-	//THINK - if the parameter is a String, or the return type is a String
-	//How can I convert to a String from an Object?
-	//What utility methods do I have available?
-	
-	//You must complete this section using TDD
-	//You can use the suggested tests or build your own.
+
+	JSONUtil jsonutil = new JSONUtil();
+
+	// You must provide concrete implementation for each of these methods
+	// do not change the method signature
+	// THINK - if the parameter is a String, or the return type is a String
+	// How can I convert to a String from an Object?
+	// What utility methods do I have available?
+
+	// You must complete this section using TDD
+	// You can use the suggested tests or build your own.
 
 	public String getAllAccounts() {
 		// TODO Auto-generated method stub
-		return null;
+		return accountMap.toString();
 	}
 
 	public String createAccount(String account) {
 		// TODO Auto-generated method stub
+		Account newAccount = jsonutil.getObjectForJSON(account, Account.class);
+		accountMap.put(newAccount.getId(), newAccount);
 		return null;
 	}
 
 	public String deleteAccount(int accountNumber) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stu
+		accountMap.remove(accountNumber);
 		return null;
 	}
 
 	public String updateAccount(int accountNumber, String account) {
 		// TODO Auto-generated method stub
+		Account updateAccount = jsonutil.getObjectForJSON(account, Account.class);
+		accountMap.put(accountNumber, updateAccount);
 		return null;
 	}
 
